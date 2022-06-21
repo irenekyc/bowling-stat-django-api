965f230f7749ee6a4b82dcd6083b8e90cd8e2551
irenekyc <gugu_irene@hotmail.com>
2022-06-22 03:38:09 +0800

    fixed django heroku settings

--- bowlingstat/settings.py
+++ bowlingstat/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import django_on_heroku
 
-django_on_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,3 +115,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+django_on_heroku.settings(locals())
