45acc1b2ce6ada6513c3bd811c91ceebd518c396
irenekyc <gugu_irene@hotmail.com>
2022-06-22 03:08:44 +0800

    Remove a hardcoded Django secret key

--- bowlingstat/settings.py
+++ bowlingstat/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*tonhty$_c3zlvhviakbgx6@)(#sm+!)miqpee%(1(oqx9%q11"
+# SECRET_KEY = "django-insecure-*tonhty$_c3zlvhviakbgx6@)(#sm+!)miqpee%(1(oqx9%q11"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
