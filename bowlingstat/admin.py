dde339813013dc1e05ee4f7b8eec5349cccf01f5
irenekyc <gugu_irene@hotmail.com>
2022-06-23 02:58:22 +0800

    added admin registration

--- bowlingstat/admin.py
+++ bowlingstat/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Team
+from .models import Team, EventData, EventSummaryData
 
 admin.site.register(Team)
+admin.site.register(EventData)
+admin.site.register(TeaEventSummaryData)
