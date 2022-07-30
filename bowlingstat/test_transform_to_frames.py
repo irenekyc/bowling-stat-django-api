952745b066e6f7ecb949607093f69e76cecdca35
irenekyc <gugu_irene@hotmail.com>
2022-07-31 02:44:27 +0800

    added tests to transform frames function

--- bowlingstat/test_transform_to_frames.py
+++ bowlingstat/test_transform_to_frames.py
@@ -66,3 +66,137 @@ class TestTransformToFrames(TestCase):
             self.assertEqual(frame_data["open"], bakerGameFiveIsOpen[i])
             # is fill ball
             self.assertEqual(frame_data["is_fill_ball"], bakerGameFiveIsFill[i])
+
+    def test_transform_to_frames_team(self):
+        ###### REGULAR GAME #######
+        ### Team ###
+        result_frames = transform_to_frames(_data=test_data_team, _game_type="Team", num_baker_games=1, num_of_baker_games_per_block=5, baker_match_play_1=7, baker_match_play_2=0, baker_match_play_3=0, game_group=0, event_type="Regular")
+        # General #
+        num_team_game = 2
+        team_member = 5
+        self.assertEqual(len(result_frames), team_member * num_team_game * 12)
+        # Team game one data #
+        teamGameOneData = result_frames.iloc[0:12]
+        teamGameOneBowler = "Test BowlerD"
+        for i in range(0, 12):
+            frame_data = teamGameOneData.to_dict(orient="records")[i]
+            # bowler
+            self.assertEqual(frame_data["bowler"], teamGameOneBowler)
+            # first ball
+            self.assertEqual(frame_data["first_ball"], teamGameOneFirstBall[i])
+            # second_ball
+            if math.isnan(frame_data["second_ball"]):
+                self.assertTrue(math.isnan(frame_data["second_ball"]))
+            else:
+                self.assertEqual(frame_data["second_ball"], teamGameOneSecondBall[i])
+            # first_ball_attempt
+            self.assertEqual(frame_data["first_ball_attempt"], teamGameOneFirstBallAttempt[i])
+            # spare
+            self.assertEqual(frame_data["spare"], teamGameOneIsSpare[i])
+            # strikes
+            self.assertEqual(frame_data["strike"], teamGameOneIsStrike[i])
+            # double
+            self.assertEqual(frame_data["double"], teamGameOneIsDouble[i])
+            # double_attempt
+            self.assertEqual(frame_data["double_attempt"], teamGameOneIsDoubleAttempt[i])
+            # open
+            self.assertEqual(frame_data["open"], teamGameOneIsOpen[i])
+            # is fill ball
+            self.assertEqual(frame_data["is_fill_ball"], teamGameOneIsFill[i])
+
+        # Team Game Six #
+        teamGameSeven = result_frames.iloc[72:84]
+        teamGameSevenBower = "Test BowlerE"
+        for i in range(0, 12):
+            frame_data = teamGameSeven.to_dict(orient="records")[i]
+            # bowler
+            self.assertEqual(frame_data["bowler"], teamGameSevenBower)
+            # first ball
+            self.assertEqual(frame_data["first_ball"], teamGameSevenFirstBall[i])
+            # second_ball
+            if math.isnan(frame_data["second_ball"]):
+                self.assertTrue(math.isnan(frame_data["second_ball"]))
+            else:
+                self.assertEqual(frame_data["second_ball"], teamGameSevenSecondBall[i])
+            # first_ball_attempt
+            self.assertEqual(frame_data["first_ball_attempt"], teamGameSevenFirstBallAttempt[i])
+            # spare
+            self.assertEqual(frame_data["spare"], teamGameSevenIsSpare[i])
+            # strikes
+            self.assertEqual(frame_data["strike"], teamGameSevenIsStrike[i])
+            # double
+            self.assertEqual(frame_data["double"], teamGameSevenIsDouble[i])
+            # double_attempt
+            self.assertEqual(frame_data["double_attempt"], teamGameSevenIsDoubleAttempt[i])
+            # open
+            self.assertEqual(frame_data["open"], teamGameSevenIsOpen[i])
+            # is fill ball
+            self.assertEqual(frame_data["is_fill_ball"], teamGameSevenIsFill[i])
+
+    def test_transform_to_frames_baker_mp(self):
+        """This is the function transform a csv row to 12 frames data
+        frames data should be correctly identified"""
+        ###### REGULAR GAME #######
+        ### Baker Match Play Version one ###
+        result_frames = transform_to_frames(_data=test_data_baker_match_play, _game_type="Baker Match Play", num_baker_games=1, num_of_baker_games_per_block=5, baker_match_play_1=7, baker_match_play_2=0, baker_match_play_3=0, game_group=0, event_type="Regular")
+        # General #
+        self.assertEqual(len(result_frames), 7 * 12)
+        # should be seven games and only one group
+        self.assertEqual(max(result_frames["game_no"]), 7)
+        self.assertEqual(max(result_frames["game_group"]), 1)
+
+        # Baker Match Play game three data #
+        bakerMPGameThree = result_frames.iloc[24:36]
+        for i in range(0, 12):
+            frame_data = bakerMPGameThree.to_dict(orient="records")[i]
+            # bowler
+            self.assertEqual(frame_data["bowler"], bakerMPGameThreeBowler[i])
+            # first ball
+            self.assertEqual(frame_data["first_ball"], bakerMPGameThreeFirstBall[i])
+            # second_ball
+            if math.isnan(frame_data["second_ball"]):
+                self.assertTrue(math.isnan(frame_data["second_ball"]))
+            else:
+                self.assertEqual(frame_data["second_ball"], bakerMPGameThreeSecondBall[i])
+            # first_ball_attempt
+            self.assertEqual(frame_data["first_ball_attempt"], bakerMPGameThreeFirstBallAttempt[i])
+            # spare
+            self.assertEqual(frame_data["spare"], bakerMPGameThreeIsSpare[i])
+            # strikes
+            self.assertEqual(frame_data["strike"], bakerMPGameThreeIsStrike[i])
+            # double
+            self.assertEqual(frame_data["double"], "-")
+            # double_attempt
+            self.assertEqual(frame_data["double_attempt"], "-")
+            # open
+            self.assertEqual(frame_data["open"], bakerMPGameThreeIsOpen[i])
+            # is fill ball
+            self.assertEqual(frame_data["is_fill_ball"], bakerMPGameThreeIsFill[i])
+
+        # Baker Match Play Game SIX #
+        bakerMPGameSix = result_frames.iloc[60:72]
+        for i in range(0, 12):
+            frame_data = bakerMPGameSix.to_dict(orient="records")[i]
+            # bowler
+            self.assertEqual(frame_data["bowler"], bakerMPGameSixBowler[i])
+            # first ball
+            self.assertEqual(frame_data["first_ball"], bakerMPGameSixFirstBall[i])
+            # second_ball
+            if math.isnan(frame_data["second_ball"]):
+                self.assertTrue(math.isnan(frame_data["second_ball"]))
+            else:
+                self.assertEqual(frame_data["second_ball"], bakerMPGameSixSecondBall[i])
+            # first_ball_attempt
+            self.assertEqual(frame_data["first_ball_attempt"], bakerMPGameSixFirstBallAttempt[i])
+            # spare
+            self.assertEqual(frame_data["spare"], bakerMPGameSixIsSpare[i])
+            # strikes
+            self.assertEqual(frame_data["strike"], bakerMPGameSixIsStrike[i])
+            # double
+            self.assertEqual(frame_data["double"], "-")
+            # double_attempt
+            self.assertEqual(frame_data["double_attempt"], "-")
+            # open
+            self.assertEqual(frame_data["open"], bakerMPGameSixIsOpen[i])
+            # is fill ball
+            self.assertEqual(frame_data["is_fill_ball"], bakerMPGameSixIsFill[i])
--- bowlingstat/testcase.py
+++ bowlingstat/testcase.py
@@ -114,7 +114,8 @@ frame_bowler_row = pd.DataFrame(
 
 test_data = pd.read_csv("bowlingstat/Test Data.csv")
 test_data_baker = test_data[test_data["GameType"] == "Baker"].iloc[0:5]
-
+test_data_team = test_data[test_data["GameType"] == "Team"]
+test_data_baker_match_play = test_data[test_data["GameType"] == "Baker"].iloc[5::]
 ## TEST CASE Baker One ##
 bakerGameOneBowler = ["Test BowlerA", "Test BowlerB", "Test BowlerC", "Test BowlerD", "Test BowlerE", "Test BowlerA", "Test BowlerB", "Test BowlerC", "Test BowlerD", "Test BowlerE", "Test BowlerE", "Test BowlerE"]
 bakerGameOneFirstBall = [9, 10, 9, 10, 9, 10, 9, 10, 0, 10, 10, 10]
@@ -133,3 +134,48 @@ bakerGameFiveIsSpare = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
 bakerGameFiveIsStrike = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0]
 bakerGameFiveIsOpen = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 bakerGameFiveIsFill = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+
+
+## TEST CASE TEAM GAME ONE (BOWLERD) ##
+teamGameOneFirstBall = [9, 10, 8, 9, 7, 7, 10, 10, 8, 5, 0, 0]
+teamGameOneSecondBall = [0, math.nan, 2, 0, 2, 1, 0, 0, 1, 4, 0, 0]
+teamGameOneFirstBallAttempt = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]
+teamGameOneIsSpare = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+teamGameOneIsStrike = [0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+teamGameOneIsDouble = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+teamGameOneIsDoubleAttempt = [0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0]
+teamGameOneIsOpen = [1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0]
+teamGameOneIsFill = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+## TEST CASE TEAM GAME SEVEN (BOWLERE) ##
+teamGameSevenFirstBall = [10, 10, 10, 0, 0, 10, 10, 9, 4, 10, 0, 0]
+teamGameSevenSecondBall = [math.nan, math.nan, math.nan, 0, 0, math.nan, math.nan, 1, 0, 0, 0, 0]
+teamGameSevenFirstBallAttempt = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+teamGameSevenIsSpare = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+teamGameSevenIsStrike = [1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0]
+teamGameSevenIsDouble = [0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+teamGameSevenIsDoubleAttempt = [0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0]
+teamGameSevenIsOpen = [0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0]
+teamGameSevenIsFill = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+## TEST CASE BAKER MP THREE ##
+bakerMPGameThreeBowler = ["Test BowlerA", "Test BowlerB", "Test BowlerC", "Test BowlerD", "Test BowlerE", "Test BowlerA", "Test BowlerB", "Test BowlerC", "Test BowlerD", "Test BowlerE", "Test BowlerE", "Test BowlerE"]
+bakerMPGameThreeFirstBall = [10, 10, 8, 6, 10, 8, 9, 6, 7, 6, 6, 0]
+bakerMPGameThreeSecondBall = [math.nan, math.nan, 2, 4, math.nan, 2, 1, 4, 3, 4, 0, 0]
+bakerMPGameThreeFirstBallAttempt = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+bakerMPGameThreeIsSpare = [0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0]
+bakerMPGameThreeIsStrike = [1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+bakerMPGameThreeIsOpen = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+bakerMPGameThreeIsFill = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+
+## TEST CASE BAKER MP SIX ##
+bakerMPGameSixBowler = ["Test BowlerA", "Test BowlerB", "Test BowlerC", "Test BowlerD", "Test BowlerE", "Test BowlerA", "Test BowlerB", "Test BowlerC", "Test BowlerD", "Test BowlerE", "Test BowlerE", "Test BowlerE"]
+bakerMPGameSixFirstBall = [9, 9, 9, 8, 8, 8, 8, 8, 8, 8, 0, 0]
+bakerMPGameSixSecondBall = [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0]
+bakerMPGameSixFirstBallAttempt = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]
+bakerMPGameSixIsSpare = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+bakerMPGameSixIsStrike = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+bakerMPGameSixIsOpen = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]
+bakerMPGameSixIsFill = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
